56c4a6863d05f91843e9957fc544b23aae7d2163
mujtaba <mujtabasadat07800@gmail.com>
2023-06-05 03:21:23 +0430

    completed the responsiveness of the website

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navList.classList.toggle("open");
+};
